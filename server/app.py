42efb17d3d6ba72f4f35b017bdcc7489939dd967
aggreyrc <aggreyronoh@gmail.com>
2024-10-01 13:43:55 +0300

    routing

--- server/app.py
+++ server/app.py
@@ -8,21 +8,14 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-# @app.route('/print/<string:parameter>')
-# def print_string(parameter):
-#     return f'<h1>{parameter}</h1>'
 
 @app.route('/print/<string:text>')
 def print_text(text):
-    print(text)  # This will print to the console
+    print(text) 
     return text
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
-    # result = ''
-    # for i in range(0, parameter):
-    #     result += f'<p>{i}</p>'
-    # return result
     return '\n'.join([str(i) for i in range(parameter)]) + '\n'
 
 @app.route('/math/<int:num1>/<path:operation>/<int:num2>')
